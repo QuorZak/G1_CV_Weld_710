23d0b665db33eb3f22f8f0267eb542ad036736b1
Zak Quor <zakquor@gmail.com>
2024-04-25 17:12:05 +1200

    Initial commit. Made skeleton/framework for project

---
+++ main.py
@@ -0,0 +1,34 @@
+###
+
+###
+
+# Import libraries
+import cv2
+import numpy as np
+
+# Do all our work, step by step, in here
+def main():
+    folder_name = "xxxx"
+    other_name1 = "xxxx"
+    other_name2 = "xxxx"
+    other_name3 = "xxxx"
+
+    # 1) read folder content and store in a list
+    image_list = []
+    
+
+    # 2) for each image in list
+    for image_name in image_list:
+        image = cv2.imread(image_name)
+
+
+    # 3) do lots of processing steps, including saving imtermediate steps
+
+
+    # 4) store results in CSV + write final image
+
+    # 5) at the end of the loop, write the CSV
+
+
+if __name__ == "__main__":
+    main()
